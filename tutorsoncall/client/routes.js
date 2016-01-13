cac25cc9bae6484ec81aebae5013c9fd2cf1c244
seng299Project <francesyu90@yahoo.com>
2016-01-13 00:23:24 -0800

    Start project by installing some packages & creating navbar

---
+++ tutorsoncall/client/routes.js
@@ -0,0 +1,11 @@
+Router.route("/",function (){
+	this.render("navbar",{
+		to:"navbar"
+	});
+	// this.render("home",{
+	// 	to:"main"
+	// });
+	// this.render("footer",{
+	// 	to:"footer"
+	// });
+});
